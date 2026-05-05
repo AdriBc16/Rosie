530fff6336a3e5da35f6683ef93e88ab1542f99b
AdriBc <adriana.bauer11@gmail.com>
2026-05-05 16:16:11 -0400

    fix:cambios

--- database/seeders/AcademicDataSeeder.php
+++ database/seeders/AcademicDataSeeder.php
@@ -76,7 +76,7 @@ class AcademicDataSeeder extends Seeder
             Materia::firstOrCreate(['nombre' => $nombreMateria]);
         }
 
-        $nombres = ['Carlos', 'Maria', 'Jose', 'Ana', 'Luis', 'Paola', 'Javier', 'Daniela', 'Miguel', 'Andrea'];
+        $nombres = ['Carlos', 'Maria', 'Jose', 'Ana', 'Luis', 'Paola', 'Javier', 'Daniela', 'Miguel', 'Andrea',"Adriana"];
         $apellidos = ['Cabrera', 'Lopez', 'Fernandez', 'Rojas', 'Vargas', 'Salazar', 'Flores', 'Mendez', 'Rivera', 'Gomez'];
 
         for ($i = 1; $i <= 20; $i++) {
